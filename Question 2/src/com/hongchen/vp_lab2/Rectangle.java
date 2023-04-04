a6a80a9d7fa0ff9b928bc4e61f4960779254572b
666RED <90583566+666RED@users.noreply.github.com>
2023-04-04 17:36:34 +0800

    Delete Question 2/src/com/hongchen/vp_lab2 directory

--- Question 2/src/com/hongchen/vp_lab2/Rectangle.java
+++
@@ -1,7 +0,0 @@
-package com.hongchen.vp_lab2;
-
-public class Rectangle extends Polygon {
-	public int calcArea() {
-		return (int)(width * height);
-	}
-}
--- Question 2/src/com/hongchen/vp_lab2/Triangle.java
+++
@@ -1,7 +0,0 @@
-package com.hongchen.vp_lab2;
-
-public class Triangle extends Polygon {
-	public int getArea() {
-		return (int)(0.5 * width * height);
-	}
-}
